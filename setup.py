c68f702619ab6d85090d148a8d378d607e2e825b
Nipun Gahane <nipungahane@gmail.com>
2025-08-09 17:53:44 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='generative_ai_project',
+    version='0.0.0',
+    author='Nipun Gahane',
+    author_email='nipungahane@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
